2ffc77f88eb0683126223ac8f8b87e396133fad6
RiddhiParikh <164143734+RiddhiParikh@users.noreply.github.com>
2024-05-09 14:40:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const passbox=document.getElementById("password");
+const length=8;
+
+const uppercase="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowercase="abcdefghijklmnopqrstuvwxyz";
+const number="0123456789";
+const symbol="@#$%^&*()_+/><{}[]-=~|"
+
+const allchar= uppercase + lowercase + number +symbol;
+
+function createpass(){
+    let password="";
+    password +=uppercase[Math.floor(Math.random() * uppercase.length)];
+    password +=lowercase[Math.floor(Math.random() * lowercase.length)];
+    password +=number[Math.floor(Math.random() * number.length)];
+    password +=symbol[Math.floor(Math.random() * symbol.length)];
+
+
+
+while(length>password.length){
+    password += allchar[Math.floor(Math.random() * allchar.length)];
+}
+passbox.value=password;
+}
+
+function copypass(){
+    passbox.select();
+    document.execCommand("copy");
+}
